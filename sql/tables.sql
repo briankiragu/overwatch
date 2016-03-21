d114a383d5b4b8abe4b5213e4d240a85565e87a8
briankiragu <bkariuki@hotmail.com>
2016-03-21 18:07:34 +0300

    Added tables to the database

--- sql/tables.sql
+++ sql/tables.sql
@@ -26,3 +26,31 @@ CREATE TABLE estates (
     name_of_estate VARCHAR(100) NOT NULL,
     location_of_estate VARCHAR(150) NOT NULL
 );
+
+CREATE TABLE reviews (
+	review_ID INT(64) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	estate_ID INT(64) NOT NULL,
+	tenant_ID INT(64) NOT NULL,
+	tenant_name VARCHAR(50) NOT NULL,
+	content VARCHAR(500) NOT NULL,
+	rating INT(1),
+	review_date DATE NOT NULL
+);
+
+CREATE TABLE messages (
+	message_ID INT(64) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	sender_ID INT(64) NOT NULL,
+	sender_name VARCHAR(100) NOT NULL,
+	receiver_ID INT(64) NOT NULL,
+	content VARCHAR(500) NOT NULL,
+	review_date DATE NOT NULL
+);
+
+CREATE TABLE updates (
+	update_ID INT(64) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	landlord_ID INT(64) NOT NULL,
+	landlord_name VARCHAR(100) NOT NULL,
+	estate_ID VARCHAR(100) NOT NULL,
+	content VARCHAR(500) NOT NULL,
+	update_date DATE NOT NULL
+);
--- templates/landlord/estates.php
+++ templates/landlord/estates.php
@@ -1,7 +1,7 @@
 <div class="col-sm-12 well">
   <div class="row">
     <div class="col-md-3">
-      <img src="img/thumbnails/photo-1438978280647-f359d95ebda4.jpg" alt="Thumbnail" class="img-responsive" >
+      <img src="img/thumbnails/photo-1438978280647-f359d95ebda4.jpg" alt="Thumbnail" class="img-responsive" />
     </div>
     <div class="col-md-9">
       <h2>{{ x.name_of_estate }} <small>{{ x.location_of_estate }}</small></h2>
@@ -10,7 +10,8 @@
       </p>
       <button class="btn btn-default" data-ng-click="viewReview()">View more</button>
     </div>
-    <div class="col-md-9 col-md-offset-3" data-ng-show="viewState">
+    <div class="col-md-9 col-md-offset-3" data-ng-show="viewState"
+     style="transition: all linear 2s">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus vel, rerum delectus rem architecto pariatur consequatur harum ipsum unde. Assumenda ea illo, ad quis! Magni quia expedita dolorum cupiditate recusandae.
     </div>
   </div>
